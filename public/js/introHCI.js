6d6aace8d811baae35609fdf337d8fc149e459a0
Brandon Lee <Brandon@fam-lee.com>
2017-02-16 12:45:33 -0800

    putting it together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,35 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("project/" + idNumber, addProject);
+	console.log(idNumber);
+
+
+	function addProject(result){
+		console.log(result); 
+		var projectHTML = '<a href="#" class="thumbnail">' + '<img src="' + result['image'] + 
+		'" class="detailsImage">' + '<p>' + result['title'] + '</p>' + result['summary'] + '<p><small>' 
+		+ result['date'] + '</small></p/a>'; 
+		$(".details", idNumber);
+		$("div #project"+ idNumber + " .details").html(projectHTML);
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result['summary']);
+	}
 }
 
-/*
- * Make an AJAX call to retrieve a color palette for the site
- * and apply it
- */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", randomColor);
+}
+
+function randomColor(result) {
+	var hex = result['colors'];
+	var colors = hex['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
